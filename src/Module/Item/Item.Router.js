d640041fc5f8e5dfa7565b2dd4bf7feb0984fae1
wasan <wasanawwad2020@gmail.com>
2024-10-28 23:09:21 +0200

    Delete Item

--- src/Module/Item/Item.Router.js
+++ src/Module/Item/Item.Router.js
@@ -6,8 +6,9 @@ const router = Router();
 
 
 router.post('/createitem',auth(EndPoints.CreateItem),ItemController.CreateItem);
-router.get('/getownitems',auth(EndPoints.GetItems),ItemController.GetOwnerItems);
+router.get('/getowneritems',auth(EndPoints.GetItems),ItemController.GetItems);
 router.put('/updateitem/:idItem',auth(EndPoints.CreateItem),ItemController.UpdateItem);
+router.delete('/deleteitem/:idItem',auth(EndPoints.CreateItem),ItemController.DeleteItem);
 
 
 export default router ;
