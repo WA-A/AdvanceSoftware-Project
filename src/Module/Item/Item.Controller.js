a60a3024b3303f147723ddbcad31049bbfc96934
SadeenHawash <133234849+SadeenHawash@users.noreply.github.com>
2024-11-01 14:48:49 +0200

    Update Item.Controller.js

--- src/Module/Item/Item.Controller.js
+++ src/Module/Item/Item.Controller.js
@@ -2,15 +2,6 @@ import ItemModel from "../../Modle/ItemModel.js";
 
 // Create New Item
 export const CreateItem = async (req, res) => {
-    const {NameItem,Description,Category,DailyPrice} = req.body
-    const Owner = req.user.id;
-    try {
-        const newItem = await ItemModel.create({NameItem,Description,Category,DailyPrice});
-        res.status(201).json({message:"Success Add Item",newItem});
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
   const { NameItem, Description, Category, DailyPrice, RentalDays } = req.body;
   const userId = req.user.id;
   if (!req.file) {
