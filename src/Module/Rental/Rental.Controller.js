add3322dc2aaebf5ff79732d0643b8ca55a8b2dc
wasan <wasanawwad2020@gmail.com>
2024-11-01 22:49:56 +0200

    Update Rental

--- src/Module/Rental/Rental.Controller.js
+++ src/Module/Rental/Rental.Controller.js
@@ -1,7 +1,9 @@
 import RentalModel from '../models/RentalModel.js';
 import ItemModel from '../models/ItemModel.js';
 
-export const createRental = async (req, res) => {
+
+// Create New Rental
+export const CreateRental = async (req, res) => {
     try {
         const { itemId } = req.params; 
         const { RentalDays, ItemTenant } = req.body; 
@@ -21,7 +23,7 @@ export const createRental = async (req, res) => {
             ItemId: itemId,
             ItemOwner,
             ItemTenant,
-            finalCost
+            FinalCost
         });
 
         res.status(201).json({ message: "Rental created successfully", FinalCost, NewRental });
@@ -29,3 +31,35 @@ export const createRental = async (req, res) => {
         res.status(500).json({ message: "Error creating rental", error });
     }
 };
+
+
+// Update Rental
+export const UpdateRental = async (req, res) => {
+    try {
+        const { rentalId } = req.params;
+        const { RentalDays, ItemTenant } = req.body;
+
+        const rental = await RentalModel.findById(rentalId);
+        if (!rental) {
+            return res.status(404).json({ message: "Rental not found" });
+        }
+
+        if (RentalDays) {
+            rental.RentalDays = RentalDays;
+            const item = await ItemModel.findById(rental.ItemId);
+            rental.FinalCost = item.DailyPrice * RentalDays;
+        }
+        if (ItemTenant) rental.ItemTenant = ItemTenant;
+
+        await rental.save();
+        res.status(200).json({ message: "Rental updated successfully", rental });
+    } catch (error) {
+        res.status(500).json({ message: "Error updating rental", error });
+    }
+};
+
+// Get Rental Own Tenant
+
+// Get Rental Own Ownar
+
+// Delete Rental 
