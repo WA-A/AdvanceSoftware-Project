675e38012b3785947cdbe54f229cdc812ba9780a
wasan <wasanawwad2020@gmail.com>
2024-11-01 23:13:58 +0200

    Delete Rental

--- src/Module/Rental/Rental.Router.js
+++ src/Module/Rental/Rental.Router.js
@@ -12,6 +12,7 @@ const router = Router();
 router.post("/createrental/:itemId",auth(EndPoints.CreateRental),RentalController.CreateRental);
 router.put("/updaterental/:rentalId",auth(EndPoints.CreateRental),RentalController.UpdateRental);
 router.get("/getrental",auth(EndPoints.CreateRental),RentalController.GetUserRentals);
+router.delete("/deleterental/:RentalId",auth(EndPoints.CreateRental),RentalController.DeleteRental);
 
 
 
